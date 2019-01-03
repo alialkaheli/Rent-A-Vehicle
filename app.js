049eb65fb0fc666150202e5d8298c59a87088eb4
Jonnatan Ortiz <ortizjs@uci.edu>
2019-01-02 19:05:36 -0800

    Fix server bug

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req,res) => res.send("Hello World"));
+app.get("/", (req,res) => res.send("Hello World!!"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
