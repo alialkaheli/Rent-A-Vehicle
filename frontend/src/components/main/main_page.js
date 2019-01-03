9ed9b0801dc4d3fe6d763022216a68c1cbbd7d5e
Jonnatan Ortiz <ortizjs@uci.edu>
2019-01-03 10:54:52 -0800

    Update main_page component

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,7 +5,7 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>Rent-A-Vehicle</h1>
+                <h3>Welcome to Rent-A-Vehicle! We hope you enjoy the joy-ride!</h3>
                 <footer>
                     An Ali, Savio and Jon production.
                 </footer>
