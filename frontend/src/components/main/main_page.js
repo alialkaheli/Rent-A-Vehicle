d1a735921d33b85a3b987087ea498a1c7153f601
Jonnatan Ortiz <ortizjs@uci.edu>
2019-01-03 10:33:03 -0800

    Add app and main page component

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Rent-A-Vehicle</h1>
+                <footer>
+                    An Ali, Savio and Jon production.
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
