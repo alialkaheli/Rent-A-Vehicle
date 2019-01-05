e41c46490cb867edfdd7109bfd432837047b085b
Jonnatan Ortiz <ortizjs@uci.edu>
2019-01-05 12:02:11 -0800

    Update root reducer and app component file

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,9 +3,13 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
+import PostsContainer from './posts/posts_container';
+import PostComposeContainer from './posts/post_compose_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import ProfileContainer from './profile/profile_container';
+
 
 const App = () => (
     <div>
@@ -14,6 +18,10 @@ const App = () => (
             <AuthRoute exact path="/" component={MainPage} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+            <ProtectedRoute exact path="/posts" component={PostsContainer} />
+            <ProtectedRoute exact path="/profile" component={ProfileContainer} />
+            <ProtectedRoute exact path="/new_post" component={PostComposeContainer} />
         </Switch>
     </div>
 );
