0feed6f9a773aa19110bb606179cd88b244812ec
srod0010 <saviofrod@gmail.com>
2019-01-05 13:56:12 -0800

    Add styling classes for image

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import '../../css_styling/splash.scss';
 
 class MainPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <h3>Welcome to Rent-A-Vehicle! We hope you enjoy the joy-ride!</h3>
-                <footer>
-                    An Ali, Savio and Jon production.
-                </footer>
+            <div className="splash-wrapper">
+                <div className="splash-image-container">
+                    <div className="splash-image">
+                        <div className="splash-text">
+                            <h1 className="splash-text-upper">Get the transportation you need</h1>
+                            <h2 className="splash-text-lower">To reach your destination</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
