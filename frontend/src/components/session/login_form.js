490078a36991531969a8d5a1abd20382515a577c
srod0010 <saviofrod@gmail.com>
2019-01-05 17:46:54 -0800

    Import login styling

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import '../../css_styling/login.scss';
 
 class LoginForm extends React.Component {
     constructor(props) {
@@ -54,25 +55,29 @@ class LoginForm extends React.Component {
 
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
+            <div className="login-wrapper">
+                <div className="login-background-img">
                     <div>
-                        <input type="text"
-                            value={this.state.email}
-                            onChange={this.update('email')}
-                            placeholder="Email"
-                        />
-                        <br />
-                        <input type="password"
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            placeholder="Password"
-                        />
-                        <br />
-                        <input type="submit" value="Submit" />
-                        {this.renderErrors()}
+                        <form onSubmit={this.handleSubmit}>
+                            <div>
+                                <input type="text"
+                                    value={this.state.email}
+                                    onChange={this.update('email')}
+                                    placeholder="Email"
+                                />
+                                <br />
+                                <input type="password"
+                                    value={this.state.password}
+                                    onChange={this.update('password')}
+                                    placeholder="Password"
+                                />
+                                <br />
+                                <input type="submit" value="Submit" />
+                                {this.renderErrors()}
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         );
     }
