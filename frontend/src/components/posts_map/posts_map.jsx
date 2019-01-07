81c57d835d08aece465cf2c7a242997f011b313e
srod0010 <saviofrod@gmail.com>
2019-01-07 09:20:34 -0800

    Initialize posts map component

---
+++ frontend/src/components/posts_map/posts_map.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router-dom';
+import '../../css_styling/00reset.scss';
+import '../../css_styling/map.scss';
+
+
+class PostsMap extends React.Component {
+    render() {
+        return(
+            <div id="map-container" ref="map">hello</div>
+        )
+    }
+}
