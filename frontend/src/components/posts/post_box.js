85a7e87a5f28c1f31308a8bf2638366f8cdbd446
ali alkaheli <alialkaheli@alis-MacBook-Pro.local>
2019-01-07 13:24:58 -0800

    Updated the way post information is getting passed in

--- frontend/src/components/posts/post_box.js
+++ frontend/src/components/posts/post_box.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class PostBox extends React.Component {
+
     render() {
-        return (
-            <div>
-                <h2>{this.props.type}</h2>
-                <h4>{this.props.description}</h4>
-                <h3>{this.props.price}</h3>
-                <h3>{this.props.pickup}</h3>
-                <h3>{this.props.startdate}</h3>
-                <h3>{this.props.enddate}</h3>
-            </div>
-        );
+        console.log(this.props.index)
+        return <div>
+            <h2>{this.props.postData.type}</h2>
+            <h4>{this.props.postData.description}</h4>
+            <h3>{this.props.postData.price}</h3>
+            <h3>{this.props.postData.pickup}</h3>
+            <h3>{this.props.postData.startdate}</h3>
+            <h3>{this.props.postData.enddate}</h3>
+            <Link to={`/posts/${this.props.index}`}>
+              Update
+            </Link>
+          </div>;
     }
 }
 
