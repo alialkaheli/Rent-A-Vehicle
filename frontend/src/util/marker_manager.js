1e1481ec52e6e08ad0dfeb8009ea043a636e3cf1
srod0010 <saviofrod@gmail.com>
2019-01-08 11:23:36 -0800

    Add update and create methods

--- frontend/src/util/marker_manager.js
+++ frontend/src/util/marker_manager.js
@@ -4,7 +4,39 @@ export default class MarkerManager {
         this.markers = {};
     }
     
-     updateMarkers(benches) {
-         console.log('time to update');
+     updateMarkers(posts) {
+
+         const postsObj = {};
+         posts.forEach(post => postsObj[post._id] = post);
+        //  console.log(posts, 'postsObj');
+         
+
+        let otherPosts = posts
+            .filter(post => !this.markers[post._id])
+            .forEach(newPost => this.createMarkerFromPost(newPost, this.handleClick));
+            // console.log(otherPosts, 'otherPosts');
+
+
+        Object.keys(this.markers)
+            .filter(postId => !postsObj[postId])
+            .forEach((postId) => this.removeMarker(this.markers[postId]));
+
+     }
+
+     createMarkerFromPost(post) {
+        //  console.log(post, 'this post is passed to createMarker');
+        const position = new window.google.maps.LatLng(37.768990,-122.426743);
+        // console.log('position is', position);
+        const marker = new window.google.maps.Marker({
+            position,
+            map: this.map,
+            postId: post._id
+        });
+        console.log(marker, `the marker is at ${marker.position} the post is ${marker.postId}`);
+       
+
+        marker.addListener('click', () => this.handleClick(post));
+        this.markers[marker.postId] = marker;
+        // console.log(this.markers);
      }
 }
