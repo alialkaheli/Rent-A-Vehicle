48994bff9cc1e5588cbd4c4b372832a7ecf87461
ali alkaheli <ali.alkaheli96@gmail.com>
2019-01-25 08:59:17 -0800

    Assigned containers name to style

--- frontend/src/components/posts/post_box.js
+++ frontend/src/components/posts/post_box.js
@@ -3,30 +3,43 @@ import React from 'react';
 import "../../css_styling/post.scss";
 
 class PostBox extends React.Component {
+    format(date){
+      const obj = date.split("-");
+      let yr = obj[0]
 
+      let day = obj[2]
+      const monthName = { "01": "January", "02": "Febuary", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December" };
+
+      obj[1] = monthName[obj[1]].toString();
+      let realtime = ""
+      realtime += obj[1] + " "+ obj[2] +", "+ obj[0]
+      return realtime
+      
+
+      
+    }
     render() {
         // console.log(this.props.index)
+      let start = this.format(this.props.postData.startdate);
+      let end = this.format(this.props.postData.enddate);
         return <div className="post-info">
-          
-          <div className="specs">
-            <div className="top-post">
-              <h3>Vehicle: {this.props.postData.type}</h3>
-              <h3>Price: $ {this.props.postData.price}</h3>
-            </div>
-            <div className="top-post">
-              <h3>Start: {this.props.postData.startdate} </h3>  
-              <h3>End: {this.props.postData.enddate} </h3>
-            </div>
-            <h3>Pick up location: {this.props.postData.pickup}</h3>
-            <h3>Description: {this.props.postData.description}</h3>
-        </div>
-          <div className="post-pic">
-            <img src={this.props.postData.photoFile} className="veh-image" />
-            {/* <h3>{this.props.postData.photoFile}</h3> */}
+            <div className="specs">
+              <h3 className="v-name">Vehicle: {this.props.postData.type}</h3>
 
-            {/* <Link to={`/posts/${this.props.index}`}>Update</Link> */}
-          </div>
-        
+              <div className="top-post">
+                <h3 className="spec-sec">{start} -   </h3>
+              <h3 className="spec-sec"> {end} </h3>
+              </div>
+            <h3 className="spec-sec">Price: ${this.props.postData.price}</h3>
+            <h3 className="spec-sec">{this.props.postData.pickup}</h3>
+            <h3 className="spec-sec">{this.props.postData.description}</h3>
+            </div>
+            <div className="post-pic">
+              <img src={this.props.postData.photoFile} className="veh-image" />
+              {/* <h3>{this.props.postData.photoFile}</h3> */}
+
+              {/* <Link to={`/posts/${this.props.index}`}>Update</Link> */}
+            </div>
           </div>;
     }
 }
