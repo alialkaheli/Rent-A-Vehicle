7bd3f7e444d08d1fd70a1def293051fc2350730c
ali alkaheli <ali.alkaheli96@gmail.com>
2019-05-27 19:43:22 -0700

    Fixed error on dates

--- frontend/src/components/posts/post_box.js
+++ frontend/src/components/posts/post_box.js
@@ -5,11 +5,11 @@ import "../../css_styling/post.scss";
 class PostBox extends React.Component {
     format(date){
       const obj = date.split("-");
-      let yr = obj[0]
+      // let yr = obj[0]
 
-      let day = obj[2]
+      // let day = obj[2]
       const monthName = { "01": "January", "02": "Febuary", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December" };
-
+      console.log(obj)
       obj[1] = monthName[obj[1]].toString();
       let realtime = ""
       realtime += obj[1] + " "+ obj[2] +", "+ obj[0]
@@ -20,8 +20,11 @@ class PostBox extends React.Component {
     }
     render() {
         // console.log(this.props.index)
-      let start = this.format(this.props.postData.startdate);
-      let end = this.format(this.props.postData.enddate);
+        let start, end;
+      if (this.props.postData.startdate && this.props.postData.enddate){
+        start = this.format(this.props.postData.startdate);
+        end = this.format(this.props.postData.enddate);
+      }
         return <div className="post-info">
             <div className="specs">
               <h3 className="v-name">Vehicle: {this.props.postData.type}</h3>
